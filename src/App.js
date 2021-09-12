32136b67f771f29be93c1c6b82e43286ca33f696
Rock <rock@cashboxng.com>
2021-09-12 12:26:09 +0100

    Add react lazy

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
+import React, { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { GiBrokenPottery } from "react-icons/gi";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Detail from "./pages/Detail";
-import Landing from "./pages/Landing";
+import Spinner from "./assets/imgs/Loader.gif";
 import Provider from "./store";
+
+const Landing = React.lazy(() => import("./pages/Landing"));
+const Detail = React.lazy(() => import("./pages/Detail"));
+
+const Loader = () => {
+  return (
+    <div>
+      <img src={Spinner} alt="fetch loader" />
+    </div>
+  );
+};
 
 function App() {
   const myErrorHandler = (error) => {
     console.log("error propagate", error);
   };
   return (
-    <ErrorBoundary
-      FallbackComponent={ErrorFallback}
-      onReset={() => {
-        // reset the state of your app so the error doesn't happen again
-      }}
-      onError={myErrorHandler}
-    >
-      <Provider>
-        <div className="App">
-          <Router>
-            <Switch>
-              <Route path="/" exact component={Landing} />
-              <Route path="/news/:id" exact component={Detail} />
-            </Switch>
-          </Router>
-        </div>
-      </Provider>
-    </ErrorBoundary>
+    <Suspense fallback={<Loader />}>
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          // reset the state of your app so the error doesn't happen again
+        }}
+        onError={myErrorHandler}
+      >
+        <Provider>
+          <div className="App">
+            <Router>
+              <Switch>
+                <Route path="/" exact component={Landing} />
+                <Route path="/news/:id" exact component={Detail} />
+              </Switch>
+            </Router>
+          </div>
+        </Provider>
+      </ErrorBoundary>
+    </Suspense>
   );
 }
 function ErrorFallback({ error, resetErrorBoundary }) {
@@ -37,7 +50,6 @@ function ErrorFallback({ error, resetErrorBoundary }) {
       <GiBrokenPottery size={100} style={{ alignSelf: "center" }} />
       <p>Something went wrong:</p>
       <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
     </div>
   );
 }
